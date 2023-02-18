7deee2e91f9e79c4517c0bd8f56ad0102a2c2270
Amgad Fahmi <amgad.fahmi@basharq.com>
2023-02-18 18:26:42 +0400

    add vite plugins to copy static files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '^/assets': {
-  //       target: 'http://localhost:5173/',
-  //     },
-  //   },
-  // },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/*',
+          dest: 'assets/',
+        },
+      ],
+    }),
+  ],
 });
